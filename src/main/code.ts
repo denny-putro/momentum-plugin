ddd47ae13a4355c04035db7f3c05006a46d4c4ec
Ackyl Mohamad <ackyl>
2023-03-06 10:19:53 +0700

    demo plugin checkpoint 1

--- src/main/code.ts
+++ src/main/code.ts
@@ -1,42 +1,62 @@
-figma.showUI(__html__, { width: 320, height: 480 });
+figma.showUI(__html__, { width: 360, height: 480 });
 
 const selectedNodes: SceneNode[] = [];
+let nodesFound: SceneNode[] = [];
+
+async function findAll(message: string) {
+  const node: PageNode = figma.currentPage;
+
+  let myPromise = new Promise<SceneNode[]>(function (resolve) {
+    setTimeout(
+      () =>
+        resolve(
+          node.findAll((node) => {
+            return node.name === message;
+          })
+        ),
+      100
+    );
+  });
+
+  return await myPromise;
+}
 
 figma.ui.onmessage = (message) => {
   if (message.type === "find-all") {
     const node: PageNode = figma.currentPage;
     let sceneNode: SceneNode[] = [];
 
-    // Find all nodes
-    node.findAll((node) => {
-      if (node.name === message.value) {
-        sceneNode.push(node);
-      }
-      return true;
-    });
-
-    // Construct node objects
-    const nodeObjects = sceneNode.map((node) => {
-      let object = {
-        id: node.id,
-        name: node.name,
-        type: node.type,
-        messageType: "search",
-      };
-      return object;
-    });
-
     // Post message to UI
     figma.ui.postMessage({
-      data: nodeObjects,
-      messageType: "search",
+      messageType: "show-loader",
+    });
+
+    findAll(message.value.toString()).then((n) => {
+      sceneNode = n;
+
+      // Save found node
+      nodesFound = sceneNode;
+
+      // Construct node objects
+      const nodeObjects = sceneNode.map((node) => {
+        let object = {
+          id: node.id,
+          name: node.name,
+          type: node.type,
+        };
+        return object;
+      });
+
+      // Post message to UI
+      figma.ui.postMessage({
+        data: nodeObjects,
+        messageType: "search",
+      });
     });
   }
 
   if (message.type === "selectNode") {
-    const selectedNode = figma.currentPage.findOne(
-      (n) => n.id === message.value
-    );
+    const selectedNode = nodesFound.find((n) => n.id === message.value);
 
     // Add or remove to selection list
     if (selectedNodes.find((n) => n === selectedNode)) {
